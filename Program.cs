8c43aadeb560e9fcae237f69a57fd99bf0efa51f
unknown <blackskiesdancer@gmail.com>
2025-08-08 16:09:38 +1000

    chess board initialised

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[,] board = new string[8, 8];
+
+        // Fill the board with empty spaces
+        for (int row = 0; row < 8; row++)
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                board[row, col] = " . ";
+            }
+        }
+
+        // Print the board to the console
+        for (int row = 0; row < 8; row++)
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                Console.Write(board[row, col]);
+            }
+            Console.WriteLine(); // Newline after each row
+        }
+    }
+}
+
